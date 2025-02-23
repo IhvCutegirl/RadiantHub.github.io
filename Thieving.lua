33d68b9671aa631a489e66a7ed31d5f689d6f5da
4DoorsMoreWhores <114468842+IhvCutegirl@users.noreply.github.com>
2025-02-23 10:02:39 +0530

    Create Thieving.lua

---
+++ Thieving.lua
@@ -0,0 +1,64 @@
+local Thieving = {}
+-- "Wood", "Blue", "Black", "Marble", "Steam Punk", "Tech", "Royal", "Purple Castle"
+function Thieving:Auto_Thieving_Selected_Function(Passed_Auto_Thieving_Selected)
+    for _, lockpick in ipairs(Passed_Auto_Thieving_Selected) do
+        if lockpick == "Wood" then
+            local args = {
+                [1] = 1355
+            }   
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+        if lockpick == "Blue" then
+            local args = {
+                [1] = 1353
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))            
+            task.wait()
+        end
+        if lockpick == "Black" then
+            local args = {
+                [1] = 1359
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+        if lockpick == "Marble" then
+            local args = {
+                [1] = 1356
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+        if lockpick == "Steam Punk" then
+            local args = {
+                [1] = 1358
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+        if lockpick == "Tech" then
+            local args = {
+                [1] = 1357
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+        if lockpick == "Royal" then
+            local args = {
+                [1] = 1354
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+        if lockpick == "Purple Castle" then
+            local args = {
+                [1] = 1360
+            }
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Thieving_Thieve"):InvokeServer(unpack(args))
+            task.wait()
+        end
+    end
+end
+
+return Thieving
