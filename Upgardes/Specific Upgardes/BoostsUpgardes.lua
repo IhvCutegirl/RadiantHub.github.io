7c60d421be215421f80cf1ee034781de6a38ab25
4DoorsMoreWhores <114468842+IhvCutegirl@users.noreply.github.com>
2025-03-13 11:10:08 +0530

    Create BoostsUpgardes.lua

---
+++ Upgardes/Specific Upgardes/BoostsUpgardes.lua
@@ -0,0 +1,20 @@
+local Boosts_Upgrades_Module = {}
+
+local Boosts_Upgrades_Folder = game:GetService("ReplicatedStorage").__DIRECTORY.Upgrades.Root["Faster Egg Open"]["Faster Egg Open 2"].Inventory.Fruit
+local Boosts_Upgrades = {}
+
+for _, descendant in ipairs(Boosts_Upgrades_Folder:GetDescendants()) do
+    if descendant:IsA("LuaSourceContainer") then
+        table.insert(Boosts_Upgrades, descendant.Name)
+    end
+end
+
+for _, upgrade in ipairs(Boosts_Upgrades) do
+    local args = {
+        [1] = upgrade
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Upgrades_Purchase"):InvokeServer(unpack(args))
+    task.wait()
+end
+
+return Boosts_Upgrades_Module
