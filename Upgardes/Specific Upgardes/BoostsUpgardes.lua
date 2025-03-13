34ad8ea12ff9da14fc649753a582e536edd4ef56
4DoorsMoreWhores <114468842+IhvCutegirl@users.noreply.github.com>
2025-03-13 11:15:30 +0530

    Update BoostsUpgardes.lua

--- Upgardes/Specific Upgardes/BoostsUpgardes.lua
+++ Upgardes/Specific Upgardes/BoostsUpgardes.lua
@@ -3,18 +3,20 @@ local Boosts_Upgrades_Module = {}
 local Boosts_Upgrades_Folder = game:GetService("ReplicatedStorage").__DIRECTORY.Upgrades.Root["Faster Egg Open"]["Faster Egg Open 2"].Inventory.Fruit
 local Boosts_Upgrades = {}
 
-for _, descendant in ipairs(Boosts_Upgrades_Folder:GetDescendants()) do
-    if descendant:IsA("LuaSourceContainer") then
-        table.insert(Boosts_Upgrades, descendant.Name)
+function Boosts_Upgrades_Module:Boosts_Upgrades_Specific()
+    for _, descendant in ipairs(Boosts_Upgrades_Folder:GetDescendants()) do
+        if descendant:IsA("LuaSourceContainer") then
+            table.insert(Boosts_Upgrades, descendant.Name)
+        end
     end
-end
 
-for _, upgrade in ipairs(Boosts_Upgrades) do
-    local args = {
-        [1] = upgrade
-    }
-    game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Upgrades_Purchase"):InvokeServer(unpack(args))
-    task.wait()
+    for _, upgrade in ipairs(Boosts_Upgrades) do
+        local args = {
+            [1] = upgrade
+        }
+        game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Upgrades_Purchase"):InvokeServer(unpack(args))
+        task.wait()
+    end
 end
 
 return Boosts_Upgrades_Module
