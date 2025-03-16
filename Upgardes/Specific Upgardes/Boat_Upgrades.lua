f73da9d3b1e700de7cc4ee1e52752e38ee195c02
4DoorsMoreWhores <114468842+IhvCutegirl@users.noreply.github.com>
2025-03-16 08:29:38 +0530

    Update Boat_Upgrades.lua

--- Upgardes/Specific Upgardes/Boat_Upgrades.lua
+++ Upgardes/Specific Upgardes/Boat_Upgrades.lua
@@ -1,6 +1,6 @@
 local Boat_Upgrades_Module = {}
 
-local Boats={"Boat_1","Boat_2","Boat_3","Boat_4","Boat_5"}
+local Boats={"Boat_2","Boat_3","Boat_4","Boat_5"}
 local Boat_Pads={"Boat_2_Pad_1","Boat_2_Pad_2","Boat_3_Pad_1","Boat_3_Pad_2","Boat_3_Pad_3","Boat_3_Pad_4","Boat_4_Pad_1","Boat_4_Pad_2","Boat_4_Pad_3","Boat_4_Pad_4","Boat_4_Pad_5","Boat_4_Pad_6","Boat_4_Pad_7","Boat_5_Pad_1","Boat_5_Pad_2","Boat_5_Pad_3","Boat_5_Pad_4","Boat_5_Pad_5","Boat_5_Pad_6","Boat_5_Pad_7","Boat_5_Pad_8","Boat_5_Pad_9","Boat_5_Pad_10","Boat_5_Pad_11","Boat_5_Pad_12","Boat_5_Pad_13","Boat_5_Pad_14"}
 
 function Boat_Upgrades_Module:Boat_Upgrades_Specific()
